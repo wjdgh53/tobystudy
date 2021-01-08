b6b14b0830aed8be63dbfdba52002b0bb55a4bb4
wjdgh53 <nohdennis@gmail.com>
2021-01-08 01:29:47 -0500

    0108 laptop

--- tobypractice/src/test/java/template/CalcSumTest.java
+++ tobypractice/src/test/java/template/CalcSumTest.java
@@ -1,4 +1,21 @@
 package template;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/junit.xml")
 public class CalcSumTest {
+    @Test
+    public void sumOfNumbers() throws IOException {
+        Calculator calculator = new Calculator();
+        int sum = calculator.calcSum(getClass().getResource("/numbers.txt").getPath());
+        assertThat(sum,is(10));
+    }
 }
